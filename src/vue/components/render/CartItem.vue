413450abc899ea77b21aa9dc8659ecb4d73f4de3
Axel González <axel@halfhelix.com>
2023-03-11 23:17:22 -0600

    Before rollup-plugin-vue

--- src/vue/components/render/CartItem.vue
+++ src/vue/components/render/CartItem.vue
@@ -1,10 +1,14 @@
 <script>
-import { ref } from "vue";
+import { ref, createComponent } from "vue";
 import { useCartStore } from "@/vue/store/cart";
 import { formatProductPrice } from "@/lib/utilities";
 import { cartItemLimit } from "@/lib/store-definition";
+import Quantity from "./Quantity.vue";
 
-export default {
+export default createComponent({
+  components: {
+    Quantity,
+  },
   props: {
     line: {
       type: Object,
@@ -37,7 +41,6 @@ export default {
               (edge) => edge.id === item.value.id
             );
             item.value = newItem;
-            this.$refs.itemQuantity.focus();
           } else {
             errorMessage.value = response.statusText;
           }
@@ -71,7 +74,7 @@ export default {
       removeItem,
     };
   },
-};
+});
 </script>
 
 <template>
@@ -125,74 +128,13 @@ export default {
             class="flex items-center space-x-2 border"
             :class="{ 'pointer-events-none opacity-50': loading }"
           >
-            <button
-              type="button"
-              class="text-ink hover:text-accent2 py-3 px-1 focus:outline-none focus:text-accent1 transition duration-150 ease-in-out"
-              aria-label="decrease quantity"
-              @click="decreaseQuantity"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1"
-                stroke="currentColor"
-                class="w-10 h-10"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M19.5 12h-15"
-                />
-              </svg>
-            </button>
-            <div class="w-16 text-center h-6" v-if="loading">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-6 h-6 m-auto animate-spin"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
-                />
-              </svg>
-            </div>
-            <input
-              v-else
-              class="w-16 text-center text-ink bg-white border-ink focus:outline-none focus:ring-2"
-              type="number"
-              ref="itemQuantity"
-              v-model="item.quantity"
-              @change="updateQuantity"
-              min="1"
-              aria-label="quantity"
-            />
-            <button
-              type="button"
-              class="text-ink hover:text-accent2 py-3 px-1 focus:outline-none focus:text-accent1 transition duration-150 ease-in-out"
-              aria-label="increase quantity"
-              @click="increaseQuantity"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1"
-                stroke="currentColor"
-                class="w-10 h-10"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M12 4.5v15m7.5-7.5h-15"
-                />
-              </svg>
-            </button>
+            <quantity
+              @decreaseQuantity="decreaseQuantity"
+              @updateQuantity="updateQuantity"
+              @increaseQuantity="increaseQuantity"
+              :quantity="item.quantity"
+              :loading="loading"
+            ></quantity>
           </div>
           <div>
             <button
---
+++ src/vue/components/render/Quantity.vue
@@ -0,0 +1,99 @@
+<script>
+import { watch } from "vue";
+export default {
+  props: {
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    loading: {
+      type: Number,
+      required: false,
+      default: false,
+    },
+  },
+  setup(props) {
+    let propQuantity = props.quantity;
+    watch(props.quantity, (first, second) => {
+      console.log(
+        "Watch props.selected function called with args:",
+        first,
+        second
+      );
+      this.$refs.itemQuantity.focus();
+      // Both props are undefined so its just a bare callback func to be run
+    });
+
+    return {
+      propQuantity,
+    };
+  },
+};
+</script>
+
+<template>
+  <button
+    type="button"
+    class="text-ink hover:text-accent2 py-3 px-1 transition duration-150 ease-in-out"
+    aria-label="decrease quantity"
+    @click="$emit('decreaseQuantity')"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1"
+      stroke="currentColor"
+      class="w-10 h-10"
+    >
+      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15" />
+    </svg>
+  </button>
+  <div class="w-16 text-center h-6" v-if="loading">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6 m-auto animate-spin"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
+      />
+    </svg>
+  </div>
+  <input
+    v-else
+    class="w-16 text-center text-ink bg-white border-ink focus:outline-none focus:ring-2"
+    type="number"
+    ref="itemQuantity"
+    v-model="propQuantity"
+    @change="$emit('updateQuantity')"
+    min="1"
+    aria-label="quantity"
+  />
+  <button
+    type="button"
+    class="text-ink hover:text-accent2 py-3 px-1 transition duration-150 ease-in-out"
+    aria-label="increase quantity"
+    @click="$emit('increaseQuantity')"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1"
+      stroke="currentColor"
+      class="w-10 h-10"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  </button>
+</template>
