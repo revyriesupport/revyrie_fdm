f0e5adcf9c6a349feb0caf60a29853e10e4d7dcb
Axel González <axel@halfhelix.com>
2023-06-28 23:39:35 -0600

    Removing old js files

--- shopify/assets/cart-logic.js
+++
@@ -1,1 +0,0 @@
-class t{constructor(){}init(){console.log("Cart init")}}const a=new t;export{a};
--- shopify/assets/product.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{console.log("product-template.js!!!!!!!")});
